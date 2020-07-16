1db9d406fe0cd35d54bc6cfa9d08f753b048c8ae
naoto.moriyama <scikitmachine72@gmail.com>
2020-07-16 23:45:14 +0900

    complete chapter2.2

---
+++ main.py
@@ -0,0 +1,17 @@
+class Gear:
+    def __init__(self, chainring, cog, rim, tire):
+        self.chainring = chainring
+        self.cog = cog
+        self.rim = rim
+        self.tire = tire
+
+    def ratio(self):
+        return self.chainring / self.cog
+
+    def gear_inches(self):
+        return self.ratio() * (self.rim + (self.tire * 2))
+
+
+if __name__ == "__main__":
+    print(Gear(52, 11, 26, 1.5).gear_inches())
+    print(Gear(52, 11, 24, 1.25).gear_inches())
