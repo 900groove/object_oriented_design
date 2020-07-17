cc671db7879c729731ae10b7b30b992753c94f8a
naoto.moriyama <scikitmachine72@gmail.com>
2020-07-18 00:48:24 +0900

    complete chapter3.2

--- main.py
+++ main.py
@@ -24,10 +24,10 @@ class Wheel:
 
 
 class Gear:
-    def __init__(self, chainring: float, cog: float, wheel: Wheel):
+    def __init__(self, chainring: float, cog: float, rim: float, tire: float):
         self.__chainring = chainring
         self.__cog = cog
-        self.__wheel = wheel
+        self.__wheel = Wheel(rim, tire)
 
     @property
     def chainring(self):
@@ -44,45 +44,17 @@ class Gear:
     def ratio(self):
         return self.chainring / self.cog
 
-    def get_inches(self):
-        return self.ratio() * self.wheel.diameter()
+    def diameter(self):
+        return self.wheel.diameter()
 
-
-class ObscuringReferences:
-    def __init__(self, data):
-        self.data = data
-
-    def diameters(self):
-        return self.data[0] + (self.data[1] * 2)
+    def gear_inches(self):
+        return self.ratio() * self.diameter()
 
 
 def wheelify(data: List):
     return [Wheel(cell[0], cell[1]) for cell in data]
 
 
-class RevealingReferences:
-    def __init__(self, wheels: List):
-        self.wheels = wheelify(wheels)
-
-    @staticmethod
-    def diameter(wheel: Wheel):
-        return wheel.rim + (wheel.tire * 2)
-
-    def diameters(self):
-        return [self.diameter(wheel) for wheel in self.wheels]
-
-
 if __name__ == "__main__":
-    data = [[622, 20], [622, 23], [559, 30], [559, 40]]
-    for d in data:
-        print(ObscuringReferences(d).diameters())
-
-    print(RevealingReferences(data).diameters())
-
-    for w in wheelify(data):
-        print(Gear(32, 21, w).get_inches())
-
     wheel = Wheel(26, 1.5)
-    print(wheel.circumference())
-    print(Gear(52, 11, wheel).get_inches())
-    print(Gear(52, 11, wheel).ratio())
+    print(Gear(52, 11, wheel).gear_inches())
