5bd1c8a5081cfcc9998fe635c52882b1ab540877
naoto.moriyama <scikitmachine72@gmail.com>
2020-07-18 13:42:11 +0900

    complete chapter3.4

--- main.py
+++ main.py
@@ -3,31 +3,10 @@ from typing import List
 import math
 
 
-class Wheel:
-    def __init__(self, rim: float, tire: float):
-        self.__rim = rim
-        self.__tire = tire
-
-    @property
-    def rim(self):
-        return self.__rim
-
-    @property
-    def tire(self):
-        return self.__tire
-
-    def diameter(self):
-        return self.rim + (self.tire * 2)
-
-    def circumference(self):
-        return self.diameter() * math.pi
-
-
 class Gear:
-    def __init__(self, chainring: float, cog: float, rim: float, tire: float):
+    def __init__(self, chainring: float, cog: float):
         self.__chainring = chainring
         self.__cog = cog
-        self.__wheel = Wheel(rim, tire)
 
     @property
     def chainring(self):
@@ -37,24 +16,40 @@ class Gear:
     def cog(self):
         return self.__cog
 
-    @property
-    def wheel(self):
-        return self.__wheel
-
-    def ratio(self):
+    def ratio(self) -> float:
         return self.chainring / self.cog
 
-    def diameter(self):
-        return self.wheel.diameter()
-
-    def gear_inches(self):
-        return self.ratio() * self.diameter()
+    def gear_inches(self, diameter: float) -> float:
+        return self.ratio() * diameter
 
 
-def wheelify(data: List):
-    return [Wheel(cell[0], cell[1]) for cell in data]
+class Wheel:
+    def __init__(self, rim: float, tire: float, chainring: float, cog: float):
+        self.__rim = rim
+        self.__tire = tire
+        self.__gear = Gear(chainring, cog)
+
+    @property
+    def rim(self):
+        return self.__rim
+
+    @property
+    def tire(self):
+        return self.__tire
+
+    @property
+    def gear(self):
+        return self.__gear
+
+    def diameter(self) -> float:
+        return self.rim + (self.tire * 2)
+
+    def circumference(self) -> float:
+        return self.diameter() * math.pi
+
+    def gear_inches(self) -> float:
+        return self.gear.gear_inches(self.diameter())
 
 
 if __name__ == "__main__":
-    wheel = Wheel(26, 1.5)
-    print(Gear(52, 11, wheel).gear_inches())
+    print(Wheel(26, 1.5, 52, 11).gear_inches())
